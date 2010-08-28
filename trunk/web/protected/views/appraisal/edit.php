7cb1f4326c29cfab4ba8b78aebf5e107d8381c21
akovalyov <akovalyov@583499ec-3249-acba-014d-2d44887370c3>
2010-08-28 15:37:04 +0000

    test dialog

---
+++ trunk/web/protected/views/appraisal/edit.php
@@ -0,0 +1,18 @@
+<?php
+$this->breadcrumbs=array(
+	'Appraisals'=>array('index'),
+	$model->name=>array('view','id'=>$model->id),
+	'Update',
+);
+
+$this->menu=array(
+	array('label'=>'List Appraisal', 'url'=>array('index')),
+	array('label'=>'Create Appraisal', 'url'=>array('create')),
+	array('label'=>'View Appraisal', 'url'=>array('view', 'id'=>$model->id)),
+	array('label'=>'Manage Appraisal', 'url'=>array('admin')),
+);
+?>
+
+<h1>Update Appraisal <?php echo $model->id; ?></h1>
+
+<?php echo $this->renderPartial('_form', array('model'=>$model, 'aClient'=>$aClient)); ?>
